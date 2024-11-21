5ca5c793d53e43646c58f5e6ebbc0d605a197a2f
Thaissa Vitória <102563361+thaissavitoria@users.noreply.github.com>
2024-11-21 08:11:40 -0300

    Iniciando cliente

---
+++ ClienteRmi_V1/src/main/java/com/mycompany/clientermi_v1/ClienteRmi_V1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clientermi_v1;
+
+/**
+ *
+ * @author admlab
+ */
+public class ClienteRmi_V1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ClienteRmi_V1/src/main/java/pacote/Client.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+import java.rmi.Naming;
+import pacote.RmiInterface;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author admlab
+ */
+public class Client {
+    public static void main(String[] args){
+        try{
+            RmiInterface objRmi = (RmiInterface)Naming.lookup("rmi://127.0.0.1:6666/Servidor");
+            JOptionPane.showMessageDialog(null,objRmi.somar(100,200));
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Erro"+ e.getMessage());
+        }
+    }
+}
